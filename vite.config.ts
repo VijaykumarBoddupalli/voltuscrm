60de0a2665a1c8522677a188f127770d5158cd45
Mandapelli <159325236+Mandapelli@users.noreply.github.com>
2025-03-26 10:53:16 +0530

    Resolved merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,23 @@
-<<<<<<< HEAD
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 5000,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    tailwindcss(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
->>>>>>> personal-repo/main
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
-})
-=======
 }));
->>>>>>> personal-repo/main
